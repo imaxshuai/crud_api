29205a4c5776c335d25977d6e0727e1900a0f9f4
xushuai <229436890@qq.com>
2020-08-21 16:18:38 +0800

    init

---
+++ src/CrudApiServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Max\CrudApi;
+
+use Illuminate\Support\ServiceProvider;
+
+class CrudApiServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+//        $this->app->singleton('crud', function () {
+//            return new Admin;
+//        });
+    }
+}
